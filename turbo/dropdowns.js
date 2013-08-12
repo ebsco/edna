04eb36359f34f204b1d70d36824db333790f734e
Wylie Fisher <wylie@dukeofcheese.com>
2013-08-12 14:54:44 -0400

    dropdown stuff

--- turbo/dropdowns.js
+++ turbo/dropdowns.js
@@ -1,5 +1,13 @@
 $(function() {
-	$(document).on('click', '.dd .dd-active', function() {
+	function replaceActive(x) {
+		$(document).on('click', '.dd .dd-list-item', function() {
+			var txt = $(this).text();
+			$(this).parent('.dd-list').siblings('.dd-active').text(txt);
+		});
+	}
+	// open up the dropdown menu
+	$(document).on('click', '.dd-active', function() {
 		$(this).toggleClass('open');
-	})
+		replaceActive();
+	});
 });
