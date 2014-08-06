611605614c37836f0b0cdd0e454b91a5e571287b
Wylie Fisher <WFisher@ebsco.com>
2014-08-06 15:48:31 -0400

    Rework some JS to work better for multiple pages

--- styleguide/scripts/script.js
+++ styleguide/scripts/script.js
@@ -1,8 +1,34 @@
 $(function() {
 
-    // add active class to clicked
+    var loadPg = function(navTitle) {
+        // load the correct content
+        $.get(navTitle + '.html', function(data) {
+            $('.evt-load').html(data);
+            $('.sg-head-h1').text('Edna Styleguide - ' + navTitle);
+            window.location.hash = navTitle.toLowerCase();
+        });        
+    }
+    
+    // get a url hash
+    var loc = window.location.hash;
+
+    // do stuff on nav click
     $(document).on('click', '.evt-active', function() {
-        $(this).addClass('active').siblings().removeClass('active');    
+
+        // get the title of the href clicked
+        var navTitle = $(this).children('a').attr('title');
+
+        loadPg(navTitle);
+
+        // add the active class
+        $(this).addClass('active').siblings().removeClass('active');
+        
     })
+
+    // if reloading page look for hash
+    if(loc) {
+        var loc = loc.split('#');
+        loadPg(loc[1]);
+    }
 
 });
