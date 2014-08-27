6d164091547ab6a8903305866799aa3d430a6b96
Wylie Fisher <WFisher@ebsco.com>
2014-08-26 21:06:48 -0400

    revised slide-panel styles. new mixin for the slide-panels. updated JS and template

--- codeguide/scripts/slide-panel.js
+++ codeguide/scripts/slide-panel.js
@@ -3,10 +3,17 @@ $(function() {
     $(document).on('click', '.evt-respond', function() {
         var btnTxt = $(this).text();
         var btnTxtSplit = btnTxt.split(' ');
+        if( btnTxtSplit[0] == 'Top') {
+            $('.mq-pnl').toggleClass('mq-pnl-open-t');
+        }
         if( btnTxtSplit[0] == 'Right') {
-            $('.panel-row').toggleClass('open-right');
-        } else {
-            $('.panel-row').toggleClass('open-left');
+            $('.mq-pnl').toggleClass('mq-pnl-open-r');
+        }
+        if( btnTxtSplit[0] == 'Bottom') {
+            $('.mq-pnl').toggleClass('mq-pnl-open-b');
+        }
+        if( btnTxtSplit[0] == 'Left') {
+            $('.mq-pnl').toggleClass('mq-pnl-open-l');
         }
     });
 
