252f05d24dcc7f57fe56c46ec7929667f3f54179
Wylie Fisher <WFisher@ebsco.com>
2014-08-28 14:26:08 -0400

    codeguide changes

--- codeguide/scripts/script.js
+++ codeguide/scripts/script.js
@@ -18,11 +18,41 @@ $(function() {
 
 	var loadPg = function(navTitle, value) {
 	    $.get('pages/' + navTitle + '.html', function(data) {
+
 			var navTxt = $('[title=' + navTitle + ']').text();
 			$('.cg-head-h1 a').text('Edna CodeGuide' + ' - ' + navTxt);
 			$('[title=' + navTitle + ']').parent().addClass('active').siblings().removeClass('active');
 			window.location.hash = navTitle.toLowerCase();
-			var template = Handlebars.compile(data);
+			// var template = Handlebars.compile(data);
+			var source = data;
+			var template = Handlebars.compile(source); 
+			var data = { 
+			    example: [ {
+			    	count: {
+			    		one: "One",
+			    		two: "Two",
+			    		three: "Three",
+			    		four: "Four",
+			    		five: "Five"
+			    	},
+			    	button: {
+			    		main: "Button",
+			    		click: "Click Me",
+			    		group: "Button Group",
+			    		select: "Select",
+			    		search: "Search"
+			    	},
+			    	list: "Item",
+			    	heading: "Heading",
+			    	lorem: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lorem lectus, pellentesque in metus molestie, adipiscing tincidunt erat. Curabitur a nibh nunc. Vestibulum dignissim vulputate tortor at mattis. Vivamus ut odio nunc. Duis a aliquet ante. Suspendisse porta eu sem eu adipiscing. Ut tempus eleifend turpis vel viverra. Duis nulla lacus, ultricies sed blandit cursus, sollicitudin id felis.",
+			    	placeholder: "placeholder text",
+			    	tag: "Level",
+			    	comment: {
+			    		html: "more markup here"
+			    	}
+			    } ]
+			}; 
+
 			$(".evt-load").html(template(data));
 			prettyPrint();
 		});
