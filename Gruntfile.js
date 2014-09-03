e26345ddf4d5f74099295d742bc4100564236943
Wylie Fisher <WFisher@ebsco.com>
2014-09-03 12:27:31 -0400

    finally fixing the build?

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,6 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-
-		shell: {
-			listFolders: {
-				stdout: false,
-				command: [
-					'rm .git/hooks/pre-push'
-				].join('&&')
-			}
-		},
 		express: {
 			server: {
 				options: {
