2167aad6cd19db0e1648c03eb093b345c5b482e1
Wylie Fisher <WFisher@ebsco.com>
2014-09-04 15:40:13 -0400

    reworking the panels

--- codeguide/scripts/slide-panel.js
+++ codeguide/scripts/slide-panel.js
@@ -1,20 +1,38 @@
-$(function() {
+window.onload = init;
+function init() {
+    var links = document.querySelectorAll('.evt-respond');
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = runIt;
+    }
+}
 
-    $(document).on('click', '.evt-respond', function() {
-        var btnTxt = $(this).text();
-        var btnTxtSplit = btnTxt.split(' ');
-        if( btnTxtSplit[0] == 'Top') {
-            $('.mq-pnl').toggleClass('mq-pnl-open-t');
+function runIt(e) {
+    var btnTxt = e.srcElement.innerText;
+    switch(btnTxt) {
+        case 'Top':
+            var side = 't';
+            break;
+        case 'Right':
+            var side = 'r';
+            break;
+        case 'Bottom':
+            var side = 'b';
+            break;
+        case 'Left':
+            var side = 'l';
+            break;
+    }
+    var mainPanel = document.getElementsByClassName('mq-pnl');
+    for (var i = 0; i < mainPanel.length; i++) {
+        var blam = mainPanel[i].className;
+        var newBlam = blam.split(' ');
+        for (var j = 0; j < newBlam.length; j++) {
+            var tst = newBlam[j];
+            var newTst = tst.split('-');
+            if(newBlam[2]) {
+                $(this).parent('.slide-nav').siblings('.mq-pnl').removeClass(newBlam[2]);
+            }
         }
-        if( btnTxtSplit[0] == 'Right') {
-            $('.mq-pnl').toggleClass('mq-pnl-open-r');
-        }
-        if( btnTxtSplit[0] == 'Bottom') {
-            $('.mq-pnl').toggleClass('mq-pnl-open-b');
-        }
-        if( btnTxtSplit[0] == 'Left') {
-            $('.mq-pnl').toggleClass('mq-pnl-open-l');
-        }
-    });
-
-});
+    }
+    $(this).parent('.slide-nav').siblings('.mq-pnl').toggleClass('mq-pnl-open-' + side);
+}
