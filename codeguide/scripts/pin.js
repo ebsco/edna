2ef6a8e93f3825f6227ec3fe3d6db837ddfacd90
Wylie Fisher <WFisher@ebsco.com>
2014-09-11 12:31:12 -0400

    some pinned element styles

--- codeguide/scripts/pin.js
+++ codeguide/scripts/pin.js
@@ -5,14 +5,29 @@ $(function() {
 	var pinTopPos = Math.ceil(pinPos.top);
 
 	$(window).on('scroll', function() {
+		var pinW = pin.width();
+
 		var scrollTop = $(window).scrollTop();
 		elemPosFixed = pinTopPos - scrollTop;
 
 		if( elemPosFixed > 0 ) {
-			pin.removeClass('fixed')
+			pin.removeClass('fixed').css('width', 'auto');
 		} else {
-			pin.addClass('fixed');
+			pin.addClass('fixed').css('width', pinW);
 		}
+
+	});
+
+	$(window).bind('resize', function(e) {
+		window.resizeEvt;
+		$(window).resize(function() {
+			clearTimeout(window.resizeEvt);
+			window.resizeEvt = setTimeout(function() {
+				var pinW = pin.parent('.col').width();
+				console.log('blam = ' + pinW);
+				pin.css('width', pinW);
+			}, 250);
+		});
 	});
 
 });
