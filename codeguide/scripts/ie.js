9b0bd7bc7fca26a4b162d63951e423c17f0cb3a1
Wylie Fisher <WFisher@ebsco.com>
2014-10-02 13:06:39 -0400

    removing a file

--- codeguide/scripts/ie.js
+++
@@ -1,8 +0,0 @@
-// var ua = window.navigator.userAgent;
-// alert( 'ua: ' + ua );
-// var msie = ua.indexOf('MSIE ');
-// if (msie > 0) {
-// 	alert( 'boom! ' );
-// }
-
-// document.getElementByTagName
