5e53fd062aca4063325a2969660c27b4412da063
Wylie Fisher <WFisher@ebsco.com>
2014-10-02 10:26:34 -0400

    fixing some styles with the layout, they were being blown out in NRC+ on the Reference page

---
+++ codeguide/scripts/ie.js
@@ -0,0 +1,8 @@
+// var ua = window.navigator.userAgent;
+// alert( 'ua: ' + ua );
+// var msie = ua.indexOf('MSIE ');
+// if (msie > 0) {
+// 	alert( 'boom! ' );
+// }
+
+// document.getElementByTagName
