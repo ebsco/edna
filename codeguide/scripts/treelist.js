829f8b8e107a42504966db437fedcf787d67891d
Wylie Fisher <WFisher@ebsco.com>
2014-11-06 12:10:25 -0500

    removing the old codeguide

--- codeguide/scripts/treelist.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-    // remove active class from treelist
-    $(document).on('click', '.list-item .txt-link', function() {
-        $('.list-item').removeClass('active');
-        $(this).parent().addClass('active');
-    });
-
-});
