0b8117dc53afc74baa935784c8b553090ea43b4e
Wylie Fisher <WFisher@ebsco.com>
2015-07-27 13:50:53 -0400

    removing an NPM task

--- Gruntfile.js
+++ Gruntfile.js
@@ -178,7 +178,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-eis-release');
 	grunt.loadNpmTasks('grunt-express');
 	grunt.loadNpmTasks('grunt-grunticon');
 	grunt.loadNpmTasks('grunt-kss');
