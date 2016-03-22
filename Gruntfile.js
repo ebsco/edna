748b546a915cb20bda75921a92e1283302e4d9f7
unknown <gjjones@TB10-GJJONES.corp.epnet.com>
2016-03-22 13:42:53 -0400

    Removes kss from edna.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,6 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		express: {
-			server: {
-				options: {
-					port: 8030,
-					host: 'http://localhost',
-					bases: 'kss-docs/',
-					debug: true
-				}
-			}
-		},
 		less: {
 			base: {
 				options: {
@@ -34,26 +24,6 @@ module.exports = function(grunt) {
 				},
 				src: 'edna.ie.less',
 				dest: 'edna.ie.css'
-			},
-			kss: {
-				options: {
-					sourceMap: true,
-					outputSourceFiles: true,
-					sourceMapURL: 'edna.css.map',
-					sourceMapFilename: 'kss-docs/public/edna.css.map'
-				},
-				src: 'edna.less',
-				dest: 'kss-docs/public/edna.css'
-			},
-			kssIEStyles: {
-				options: {
-					sourceMap: true,
-					outputSourceFiles: true,
-					sourceMapURL: 'edna.ie.css.map',
-					sourceMapFilename: 'kss-docs/public/edna.ie.css.map'
-				},
-				src: 'edna.ie.less',
-				dest: 'kss-docs/public/edna.ie.css'
 			}
 		},
 		cssmin: {
@@ -63,27 +33,7 @@ module.exports = function(grunt) {
 					'edna.ie.min.css': 'edna.ie.css'
 				}
 			}
-		},
-		watch: {
-			dev: {
-				files: ['*.less', 'less/*.less', 'Gruntfile.js'],
-				tasks: ['less:base', 'less:ieStyles'],
-				options: {
-					livereload: true
-				}
-			},
-			dist: {
-				files: ['*.less', 'less/*.less', 'Gruntfile.js'],
-				tasks: ['less:base', 'less:ieStyles', 'cssmin'],
-				options: {
-					livereload: true
-				}
-			},
-			kss: {
-				files: ['**/*.less', 'less/styleguide.md'],
-				tasks: ['kss', 'less:kss', 'less:kssIEStyles'],
-			}
-		},
+		}
 		grunticon: {
 			options: {
 				datasvgcss: 'css/icons.svg.css',
@@ -106,14 +56,6 @@ module.exports = function(grunt) {
 					cwd: 'grunticon/raw',
 					src: ['*.svg'],
 					dest: 'grunticon'
-				}]
-			},
-			kss: {
-				files: [{
-					expand: true,
-					cwd: 'grunticon/raw',
-					src: ['*.svg'],
-					dest: 'kss-docs/public'
 				}]
 			}
 		},
@@ -139,76 +81,17 @@ module.exports = function(grunt) {
 					csslintrc: '.csslintrc'
 				}
 			}
-		},
-		kss: {
-			default: {
-				options: {
-					template: 'kss-template'
-				},
-				files: {
-					'kss-docs': ['less']
-				}
-			}
-		},
-		browserSync: {
-			default: {
-				bsFiles: {
-					src: [
-						'kss-docs/public/*.css',
-						'kss-docs/*.html'
-					]
-				},
-				options: {
-					server: {
-						baseDir: 'kss-docs'
-					},
-					host: 'localhost',
-					port: 8030,
-					watchTask: true
-				}
-			}
 		}
 
 	});
 
-	grunt.loadNpmTasks('grunt-browser-sync');
 	grunt.loadNpmTasks('grunt-colorguard');
 	grunt.loadNpmTasks('grunt-contrib-analyze-css');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-express');
 	grunt.loadNpmTasks('grunt-grunticon');
-	grunt.loadNpmTasks('grunt-kss');
 	grunt.loadNpmTasks('grunt-shell');
-
-	grunt.registerTask('kss-build', [
-		'kss',
-		'grunticon:kss',
-		'less:kss',
-		'less:kssIEStyles',
-	]);
-
-	grunt.registerTask('kss-dev', [
-		'kss-build',
-		'browserSync',
-		'watch:kss',
-	]);
-
-	grunt.registerTask('server', [
-		'express',
-		'grunticon:default',
-		'less',
-		'cssmin',
-		'watch'
-	]);
-
-	grunt.registerTask('server-deploy', [
-		'kss-build',
-		'express',
-		'express-keepalive'
-	]);
 
 	grunt.registerTask('build', function(mode) {
 		var tasksToRun = [];
